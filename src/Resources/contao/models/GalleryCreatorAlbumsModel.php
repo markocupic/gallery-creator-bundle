2501035dd9e27ae1418e2baecc9a16919a5ee002
Marko Cupic <m.cupic@gmx.ch>
2016-12-24 13:27:37 +0100

    dev

--- src/Resources/contao/models/GalleryCreatorAlbumsModel.php
+++ src/Resources/contao/models/GalleryCreatorAlbumsModel.php
@@ -13,7 +13,12 @@
 /**
  * Run in a custom namespace, so the class can be replaced
  */
-namespace Markocupic;
+namespace Contao;
+
+use Contao\GalleryCreatorPicturesModel;
+use Markocupic\GcHelpers;
+
+
 
 /**
  * Reads and writes tl_gallery_creator_albums
--- src/Resources/contao/models/GalleryCreatorPicturesModel.php
+++ src/Resources/contao/models/GalleryCreatorPicturesModel.php
@@ -14,7 +14,11 @@
 /**
  * Run in a custom namespace, so the class can be replaced
  */
-namespace Markocupic;
+namespace Contao;
+
+use Contao\GalleryCreatorAlbumsModel;
+use Markocupic\GcHelpers;
+
 
 /**
  * Reads and writes tl_gallery_creator_pictures

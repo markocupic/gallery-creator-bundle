2c6a6125abc35c2f40aca24abdfd8235c7cfc252
Marko Cupic <m.cupic@gmx.ch>
2017-01-01 23:06:50 +0100

    added ide_compat.php

---
+++ src/Resources/contao/config/ide_compat.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+// This file is not used in Contao. Its only purpose is to make PHP IDEs like
+// Eclipse, Zend Studio or PHPStorm realize the class origins, since the dynamic
+// class aliasing we are using is a bit too complex for them to understand.
+namespace  {
+	class GalleryCreatorAlbumsModel extends \Contao\GalleryCreatorAlbumsModel {}
+	class GalleryCreatorPicturesModel extends \Contao\GalleryCreatorPicturesModel {}
+	class ContentGalleryCreator extends \Markocupic\GalleryCreatorBundle\ContentGalleryCreator {}
+	class ContentGalleryCreatorNews extends \Markocupic\GalleryCreatorBundle\ContentGalleryCreatorNews {}
+	class GcHelpers extends \Markocupic\GalleryCreatorBundle\GcHelpers {}
+	class InstallConfig extends \Markocupic\GalleryCreatorBundle\InstallConfig {}
+}

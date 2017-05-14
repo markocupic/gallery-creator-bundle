624623f4c30c4d4a217caf910af7388dac12b4b8
Marko Cupic <m.cupic@gmx.ch>
2017-05-14 10:31:40 +0200

    Updated extension for use with ContaoManager

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright  Marko Cupic 2017 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    GalleryCreatrBundle
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/markocupic/gallery-creator-bundle
+ *
+ */
+namespace Markocupic\GalleryCreatorBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Marko Cupic
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Markocupic\GalleryCreatorBundle\MarkocupicGalleryCreatorBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}

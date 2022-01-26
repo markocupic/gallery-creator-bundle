06d23519299e0d8ad3a53a49df941e99ca6b3aa7
Marko Cupic <m.cupic@gmx.ch>
2022-01-26 20:30:12 +0100

    Do not show child albums in news elements

--- src/Controller/ContentElement/AbstractGalleryCreatorController.php
+++ src/Controller/ContentElement/AbstractGalleryCreatorController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Gallery Creator Bundle.
  *
- * (c) Marko Cupic 2021 <m.cupic@gmx.ch>
+ * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
@@ -105,8 +105,12 @@ abstract class AbstractGalleryCreatorController extends AbstractContentElementCo
         $arrCssClasses[] = GalleryCreatorAlbumsModel::hasChildAlbums((int) $albumModel->id) ? 'gc-has-child-album' : null;
         $arrCssClasses[] = !$countPictures ? 'gc-empty-album' : null;
 
-        // Do not show child albums, if they are not allowed/selected
-        $childAlbums = $this->getChildAlbums($albumModel, $contentElementModel, true);
+        // Do not show child albums, in news elements
+        if (GalleryCreatorNewsController::TYPE !== $contentElementModel->type) {
+            $childAlbums = null;
+        } else {
+            $childAlbums = $this->getChildAlbums($albumModel, $contentElementModel, true);
+        }
 
         $childAlbumCount = null !== $childAlbums ? \count($childAlbums) : 0;
 
